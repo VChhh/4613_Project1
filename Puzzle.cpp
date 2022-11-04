20b3e253a2b0592d0cca88a4d9498f1923d5c471
DrPeachy <p1067838263@gmail.com>
2022-11-04 18:46:08 -0400

    a

--- Puzzle.cpp
+++ Puzzle.cpp
@@ -1,37 +1,99 @@
 #include "Puzzle.h"
 
-Puzzle::Puzzle(std::vector<std::vector<unsigned char>> _board, std::vector<unsigned char> _goal) {
-	board = _board;
-	goal = _goal;
-	heuristic = 0;
-	for (size_t i = 0; i < 4; ++i) {
-		for (size_t j = 0; j < 4; ++j) {
-			heuristic += std::max(abs(int(_goal[_board[i][j]] / 4 - i)), abs(int(_goal[_board[i][j]] % 4 - j)));
-		 }
+using namespace std;
+
+
+
+void State::calculateHeuristic(const State& g) {
+	int res = 0;
+	for (const Chess& i : board) {
+		for (const Chess& j : g.board) {
+			if (i.value == j.value) {
+				res += max(abs(i.x - j.x), abs(i.y - j.y));
+			}
+		}
 	}
+	heuristicValue = res;
 }
 
-void Puzzle::operator=(const Puzzle& rhs) {
-	board = rhs.board;
+bool State::operator==(const State& rhs) {
+	for (int i = 0; i < 16; i++) {
+		if (board[i].value != rhs.board[i].value) {
+			return false;
+		}
+	}
+	return true;
+}
+
+// constructor store weight, init, and goal from input
+Puzzle::Puzzle(int aWeight, vector<int>& aInit, vector<int>& aGoal): weight(aWeight) {
+	cur = State();
+	goal = State();
+	for (int y = 0; y < 4; y++) {
+		for (int x = 0; x < 4; x++) {
+			cur.board.push_back(Chess(aInit[x + y * 4], x, y));
+			goal.board.push_back(Chess(aGoal[x + y * 4], x, y));
+		}
+	}
+	cur.calculateHeuristic(goal);
+	goal.calculateHeuristic(goal);
+	queue.push(cur);
+}
+
+Puzzle::Puzzle(const Puzzle& rhs) {
+	queue = rhs.queue;
+	visited = rhs.visited;
+	cur = rhs.cur;
 	goal = rhs.goal;
-	heuristic = rhs.heuristic;
+	weight = rhs.weight;
 }
 
-void Puzzle::left() {
+bool Puzzle::isVisited(State& p1) {
+	for (State& p2 : visited) {
+		if (p1 == p2) {
+			return true;
+		}
+	}
+	return false;
+}
+string Puzzle::solve() {
+	//is-goal
+	if (cur.heuristicValue == 0) return cur.path;
+	while (!queue.empty()) {
+		cur = queue.top();
+		visited.push_back(cur);
+		queue.pop();
+		std::vector<State> neighbors;
+		for (State& n : neighbors) {
+			if (!isVisited(n)) {
+
+			}
+		}
+	}
 
 }
-void Puzzle::right() {
 
-}
+//void Puzzle::operator=(const Puzzle& rhs) {
+//	board = rhs.board;
+//	goal = rhs.goal;
+//	heuristic = rhs.heuristic;
+//}
 
-void Puzzle::up() {
-
-}
-
-void Puzzle::down() {
-
-} 
-
-void Puzzle::solve(std::vector<std::string>& path, int& d) {
-
-}
+//void Puzzle::left() {
+//
+//}
+//void Puzzle::right() {
+//
+//}
+//
+//void Puzzle::up() {
+//
+//}
+//
+//void Puzzle::down() {
+//
+//} 
+//
+//void Puzzle::solve(std::vector<std::string>& path, int& d) {
+//
+//}
--- Puzzle.h
+++ Puzzle.h
@@ -1,21 +1,54 @@
-#pragma once
-
 #include<vector>
 #include<string>
+#include<queue>
+
+#ifndef PUZZLE_H
+#define PUZZLE_H
+
+
+
+struct Chess {
+	int value;
+	int x;
+	int y;
+	Chess(int v, int x, int y) : value(v), x(x), y(y) {};
+};
+
+struct State {
+	int heuristicValue;
+	int stepTaken;
+	int fValue;
+	std::vector<Chess> board;
+	std::string path;
+
+	State() : heuristicValue(16), stepTaken(0), fValue(0), path("") {};
+	State(int h, std::vector<Chess> c, int s, int f) : heuristicValue(h), board(c), stepTaken(s), fValue(f) {};
+	void calculateHeuristic(const State&);
+	inline bool operator==(const State&);
+};
+
+struct CompareState {
+	bool operator()(State const& p1, State const& p2)
+	{
+		return p1.fValue < p2.fValue;
+	}
+};
 
 class Puzzle{
 public:
-	Puzzle(std::vector<std::vector<unsigned char>>, std::vector<unsigned char>);
-	void operator=(const Puzzle& rhs);
-	void solve(std::vector<std::string>&, int& d);
-
-	void left();
-	void right();
-	void up();
-	void down();
-
+	Puzzle(int aWeight, std::vector<int>& aInit, std::vector<int>& aGoal);
+	Puzzle(const Puzzle&);
+	bool isVisited(State&);
+	std::string solve();
+	
 private:
-	std::vector<std::vector<unsigned char>> board;
-	std::vector<unsigned char> goal; // 0 - 15 represents the position on the board
-	unsigned char heuristic; // the heuristic value of current state
+	std::priority_queue<State, std::vector<State>, CompareState> queue;
+	std::vector<State> visited;
+	State cur;
+	State goal; // 0 - 15 represents the position on the board
+	int weight;
+	
 };
+
+
+#endif // !PUZZLE_H
